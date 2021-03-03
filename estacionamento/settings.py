6e365485567ca152a28e69b95c626eb30c0a8d82
Fukushiro <jpflc2301@gmail.com>
2021-03-03 15:08:28 -0300

    novo commit

--- estacionamento/settings.py
+++ estacionamento/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['localhost', 'dangoteste.herokuapp.com',
-                 'estacionamentoapp3.herokuapp.com/']
+                 'estacionamentoapp3.herokuapp.com']
 
 
 # Application definition
